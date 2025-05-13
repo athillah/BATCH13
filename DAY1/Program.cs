256e4bc04397a66ba4da2dbc18781752cb7a26e4
athillah <syamsuddinathillah@gmail.com>
2025-05-13 11:39:39 +0700

    Logic

--- DAY1/Program.cs
+++ DAY1/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Write a simple console program that prints the number from 1 to n, for each number x:
+
+print "foo", if x is divisible by 3
+print "bar", if x is divisible by 5
+print "foobar", if x is divisible by 3 and 5
+
+print the number itself, if x satisfies none of the rule
+Here's a sample output of such program with n=15
+>> 1, 2, foo, 4, bar, foo, 7, 8, foo, bar, 11, foo, 13, 14,foobar */
+
+using System;
+
+namespace LogicExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number:");
+            int n = 15;
+            string output = "";
+            for (int i=1; i<n+1; i++)
+            {
+                Console.WriteLine("COunting " + i);
+                if (i%3==0)
+                {
+                    output += "foo";
+                }
+                if (i%5==0)
+                {
+                    output += "bar";
+                }
+                if (i%5!=0&&i%3!=0)
+                {
+                    output += i.ToString();
+                }
+                if (i != n)
+                {
+                    output += ", ";
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
--- DAY1/obj/Debug/net8.0/DAY1.AssemblyInfo.cs
+++ DAY1/obj/Debug/net8.0/DAY1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAY1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f505220b008dc5ed19f218b028a04dace1c83a36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+152e808b5a2f2ef09106c912a45934e0dfd8890a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAY1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAY1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
