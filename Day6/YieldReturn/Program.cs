7bc2368c578474f46a122a56c4c7f6903ccef115
athillah <syamsuddinathillah@gmail.com>
2025-05-20 11:17:30 +0700

    belum ada yield return

---
+++ Day6/YieldReturn/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using static System.Console;
+
+public enum Axis { A, B, C, D, E, F, G, H }
+
+public interface IPointOfInterest
+{
+    string? Tag { get; set; }
+    Axis X { get; set; }
+    Axis Y { get; set; }
+}
+
+public interface IMoveable
+{
+    void MoveHorizontal();
+    void MoveVertical();
+}
+
+public class Player : IMoveable, IPointOfInterest
+{
+    public string? Tag { get; set; }
+    public Axis X { get; set; }
+    public Axis Y { get; set; }
+    public Player(string tag)
+    {
+        Tag = tag;
+        X = Axis.A;
+        Y = Axis.A;
+    }
+    public void MoveHorizontal()
+    {
+        X = (Axis)((int)X + 1);
+    }
+    public void MoveVertical()
+    {
+        Y = (Axis)((int)Y + 1);
+    }
+}
+
+public class Mapper
+{
+    private int _x { get; set; }
+    private int _y { get; set; }
+    public Mapper(int x, int y)
+    {
+        _x = x; _y = y;
+    }
+    public void Map(List<IPointOfInterest> PoIs)
+    {
+        StringBuilder map = new StringBuilder();
+        for (int j = 0; j <= _y; j++)
+        {
+            for (int i = 0; i <= _x; i++)
+            {
+                var poi = PoIs.FirstOrDefault(p => p.X == (Axis)i && p.Y == (Axis)j);
+                if (poi != null)
+                {
+                    map.Append($"[{poi.Tag}]");
+                }
+                else
+                {
+                    map.Append("[ ]");
+                }
+            }
+            WriteLine(map.ToString());
+            map.Clear();
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Player PlayerA = new Player("A");
+        Player PlayerB = new Player("B");
+        Mapper MapperA = new Mapper(8, 8);
+
+        var points = new List<IPointOfInterest> { PlayerA, PlayerB };
+
+        MapperA.Map(points);
+        for (int i = 0; i <= 8; i++)
+        {
+            Thread.Sleep(5000);
+            Clear();
+            PlayerA.MoveHorizontal();
+            PlayerA.MoveVertical();
+            PlayerB.MoveHorizontal();
+            MapperA.Map(points);  
+        }
+    }
+}
