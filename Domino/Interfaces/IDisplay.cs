20f3fade4b504fbd3d17290a0ad29e2dc15da13b
athillah <108055049+athillah@users.noreply.github.com>
2025-06-08 21:53:15 +0700

    adjust interface

--- Domino/Interfaces/IDisplay.cs
+++ Domino/Interfaces/IDisplay.cs
@@ -22,6 +22,6 @@ public interface IDisplay
     void MainMenu();
     int PromptMenu();
     void ConfigMenu();
-    void ShowHint();
+    void ShowHint(List<ICard> cards, List<ICard> playableCards);
     void PrintHeader(string title);
 }
